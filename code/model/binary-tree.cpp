3b61bee12385a29c22bd63aaeb00d0bf93d45e55
mannerth <3502281960@qq.com>
2026-01-11 22:50:39 +0800

    1.11 22:50

--- code/model/binary-tree.cpp
+++ code/model/binary-tree.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<stack>
+#include<queue>
 using namespace std;
 
 template<class T>
@@ -36,5 +38,43 @@ public:
         root = r;
     }
 
-    void inOrder( binaryTreeNode<T>* node, void (*opt) (T d) );
+    binaryTreeNode<T>* getRoot(){
+        return root;
+    } 
+
+    /// 层次序创建完全二叉树
+    static binaryTree buildByLevelOrder(T *vals, int size){
+        binaryTree instance{};
+        if(size==0 || !vals){
+            return instance;
+        }
+
+        instance.root = new binaryTreeNode<T>(vals[0]);
+        instance.size = 1;
+
+        queue<binaryTreeNode<T>*> que;
+        que.push(instance.root);
+
+        binaryTreeNode<T>* p;
+        while( !que.empty() && instance.size < size ){
+            p = que.front();
+            que.pop();
+
+            p->left = new binaryTreeNode<T>( vals[instance.size++] );
+            que.push(p->left);
+
+            if(instance.size == size) break;
+
+            p->right = new binaryTreeNode<T>( vals[instance.size++] );
+            que.push(p->right);
+        }
+
+        return instance;
+    }
+
+    void inOrder( void (*opt) (T d) );
+
+    void door( void (*func)(binaryTree<T> ) ){
+        func(this);
+    }
 };
--- code/traverse-binary-tree.cpp
+++ code/traverse-binary-tree.cpp
@@ -5,7 +5,7 @@
 
 // 中序遍历非递归
 template<class T>
-void inOrder(binaryTreeNode<T>* node, void (*opt)(T v) ){
+void _inOrder(binaryTreeNode<T>* node, void (*opt)(T v) ){
 
     stack< binaryTreeNode<T>* > stk;
     binaryTreeNode<int>* p = node;
@@ -24,6 +24,71 @@ void inOrder(binaryTreeNode<T>* node, void (*opt)(T v) ){
     }while( p || !stk.empty() );
 
 };
+
+template<class T>
+void binaryTree<T>::inOrder( void (*opt)(T val) ){
+    _inOrder<T>(this->root, opt);
+}
+
+// 前序遍历非递归实现
+template<class T>
+void _preOrder( binaryTreeNode<T>* node, void (*func)(T val) ){
+    binaryTreeNode<T>* p = node;
+    stack< binaryTreeNode<T>* > stk;
+
+    while( !stk.empty() || p ){
+        while( p ){
+            func(p->data);
+            stk.push(p);
+            p = p->left;
+        }
+        if( !stk.empty() ){
+            p = stk.top();
+            stk.pop();
+
+            p = p->right;
+        }
+    }
+}
+
+// 后序遍历非递归
+template<class T>
+void _lastOrder( binaryTreeNode<T>* node, void (*func)(T val) ){
+    //后序遍历，先访问左右子，再访问本节点
+    //第二次相见时才访问
+
+    binaryTreeNode<T>* p = node;
+    stack< binaryTreeNode<T>* > stk;
+
+    int flag;
+    stack< int > fs;    //标志位
+
+    while( !stk.empty() || p ){
+        while( p ){
+            stk.push(p);
+            fs.push(1); //首次入栈
+            p = p->left;
+        }
+
+        if( !stk.empty() ){
+            p = stk.top();
+            flag = fs.top();
+
+            if( p->right && flag==1 ){
+                //第一次，不访问
+                fs.pop();
+                fs.push(2); //二次入栈
+                p = p->right;
+            }else{
+                //第二次或无右子，访问
+                func(p->data);
+                stk.pop();
+                fs.pop();
+                p = nullptr;
+            }
+        }
+    }
+}
 
 void print(int v){
     cout << v << endl;
@@ -60,8 +125,22 @@ int main(){
     );
 
     void (*f)(int v) = print;
-    inOrder<int>( &node, [](int a){ cout << a << endl;} );
+    _inOrder<int>( &node, [](int a){ cout << a << endl;} );
 
+    cout << "-----------" << endl;
+    cout << "前序遍历：" << endl;
+    _preOrder<int>( &node, print );
+    cout << "-----------" << endl;
+    cout << "后序遍历" << endl;
+    _lastOrder<int> (&node, print);
+    cout << "-----------" << endl;
+
+    int arr[] = {1, 2, 3 ,4, 5, 6};
+    binaryTree<int> tree = binaryTree<int>::buildByLevelOrder(arr , 6);
+    _inOrder<int>( tree.getRoot(), print );
+
+    cout << "-----------" << endl;
+    tree.inOrder( print );
 
     return 0;
 }
