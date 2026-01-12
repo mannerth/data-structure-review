8c5865aafebeafdf32f0c21a0bb9bf11756f7637
mannerth <3502281960@qq.com>
2026-01-12 22:53:03 +0800

    1.12 22:52

--- code/sort.cpp
+++ code/sort.cpp
@@ -29,8 +29,43 @@ void binSort(int* arr, int size, int range){
     delete[] bin;
 }
 
-class MyData{
+template<class T>
+void _merge(T *a, int na, T* b, int nb, T* dest){
+    int pa = 0, pb = 0, n = 0;
     
+    while(pa < na && pb < nb){
+        if(a[pa] < b[pb]){
+            dest[n++] = a[pa++];
+        }else{
+            dest[n++] = b[pb++];
+        }
+    }
+    
+    while(pa < na) dest[n++] = a[pa++];
+    while(pb < nb) dest[n++] = b[pb++];
+}
+
+template<class T>
+void mergeSort(T *arr, int n){
+    if(n <= 1){
+        return;
+    }
+    
+    int mid = n / 2;
+    mergeSort(arr, mid);
+    mergeSort(arr + mid, n - mid);
+    
+    T* temp = new T[n];
+    _merge(arr, mid, arr + mid, n - mid, temp);
+    
+    for(int i = 0; i < n; ++i){
+        arr[i] = temp[i];
+    }
+    
+    delete[] temp;
+}
+
+class MyData{
 public:
     int x;
     int y;
@@ -38,23 +73,35 @@ public:
         x = rand();
         y = rand();
     }
+
+    bool operator<(MyData o){
+        return x < y;
+    }
 };
 
 int main(){
     
-    MyData arr[5];
-    for(int i = 0; i < 5; ++i){
-        cout << arr[i].x << "," << arr[i].y << endl;
-    }
+    // MyData arr[5];
+    // for(int i = 0; i < 5; ++i){
+    //     cout << arr[i].x << "," << arr[i].y << endl;
+    // }
 
-    sort(arr, arr+5+1, 
-        [](MyData a, MyData b){
-            return a.x > b.x;
-        }
-    );
+    // // sort(arr, arr+5+1, 
+    // //     [](MyData a, MyData b){
+    // //         return a.x > b.x;
+    // //     }
+    // // );
 
+    // mergeSort(arr, 5);
+
+    // for(int i = 0; i < 5; ++i){
+    //     cout << arr[i].x << "," << arr[i].y << endl;
+    // }
+
+    int arr[] = {5, 1, 6, 11, -1};
+    mergeSort(arr, 5);
     for(int i = 0; i < 5; ++i){
-        cout << arr[i].x << "," << arr[i].y << endl;
+        cout << arr[i] << endl;
     }
     
     return 0;
